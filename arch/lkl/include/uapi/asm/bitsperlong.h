8bd0a3c5fe4c66230a9c3aa3e3ae7de31cc4005a
Akira Moroo <retrage01@gmail.com>
2018-04-18 18:53:12 +0900

    [WIP]: Modify for building with emcc

--- arch/lkl/include/uapi/asm/bitsperlong.h
+++ arch/lkl/include/uapi/asm/bitsperlong.h
@@ -1,11 +1,18 @@
 #ifndef _ASM_UAPI_LKL_BITSPERLONG_H
 #define _ASM_UAPI_LKL_BITSPERLONG_H
 
+/*
 #ifdef CONFIG_64BIT
 #define __BITS_PER_LONG 64
 #else
 #define __BITS_PER_LONG 32
 #endif
+*/
+
+/* for emscripten */
+#define __BITS_PER_LONG 32
+
+#define BITS_PER_LONG __BITS_PER_LONG
 
 #define __ARCH_WANT_STAT64
 
