cc9139ca15ffefb8f4055c514abc6cb559d0797f
Akira Moroo <retrage01@gmail.com>
2018-05-29 14:34:22 +0900

    Increase PTHREAD_POOL_SIZE from 2 to 128

--- build-boot.sh
+++ build-boot.sh
@@ -16,4 +16,4 @@ llvm-dis -o js/libc.ll js/libc.bc
 echo "PY rename_symbols.py"
 python rename_symbols.py tools/lkl/tests/boot-mod.ll tools/lkl/tests/boot-mod2.ll
 echo "EMCC boot.js"
-EMCC_DEBUG=1 emcc -o tools/lkl/tests/boot.js tools/lkl/tests/boot-mod2.ll -s EMULATE_FUNCTION_POINTER_CASTS=1 -s PTHREAD_POOL_SIZE=2 -s USE_PTHREADS=1 -v
+EMCC_DEBUG=1 emcc -o js/boot.html tools/lkl/tests/boot-mod2.ll -s EMULATE_FUNCTION_POINTER_CASTS=1 -s PTHREAD_POOL_SIZE=128 -s USE_PTHREADS=1 -v
