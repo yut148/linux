04a739c92a5ca455c470eef8e1161767d91372b3
Akira Moroo <retrage01@gmail.com>
2018-06-21 15:14:39 +0900

    Add Emscripten support to lk/arch_thread

--- tools/lkl/lib/lk/arch_thread.c
+++ tools/lkl/lib/lk/arch_thread.c
@@ -1,8 +1,14 @@
 #include <lk/kernel/thread.h>
 #include <lk/kernel/timer.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#ifdef __EMSCRIPTEN__
+#include <stdlib.h>
+#include <emscripten.h>
+#include <lk/arch/emscripten.h>
+#endif
 
 struct thread *_current_thread = NULL;
 int ints_enabled = 0;
@@ -22,6 +28,21 @@ static void initial_thread_func(void)
 }
 
 void arch_thread_initialize(struct thread* t) {
+#ifdef __EMSCRIPTEN__
+    // create a default stack frame on the stack
+    addr_t stack_top = (addr_t)t->stack + t->stack_size;
+
+    struct emscripten_context_switch_frame *frame = (struct emscripten_context_switch_frame *)(stack_top);
+
+    // move down a frame size and zero it out
+    frame--;
+    memset(frame, 0, sizeof(*frame));
+
+    frame->sp = (addr_t) &initial_thread_func;
+
+    // set the stack pointer
+    t->arch.sp = (addr_t)frame;
+#else
     // init context
     getcontext(&t->arch.context);
 
@@ -35,10 +56,27 @@ void arch_thread_initialize(struct thread* t) {
 
     // set entrypoint
     makecontext(&t->arch.context, initial_thread_func, 0);
+#endif /* __EMSCRIPTEN__ */
 }
 
 void arch_context_switch(struct thread *oldthread, struct thread *newthread) {
+#ifdef __EMSCRIPTEN__
+    // swap context
+    EM_ASM_({
+        var $vararg_buffer = 0;
+        var sp = 0;
+        sp = STACKTOP;
+        STACKTOP = STACKTOP + 4|0; if ((STACKTOP|0) >= (STACK_MAX|0)) abortStackOverflow(4|0);
+        vararg_buffer = sp + 4|0;
+        // push old sp
+        HEAP32[$vararg_buffer>>2] = $0;
+        // load new sp
+        STACKTOP = $1;
+    }, oldthread->arch.sp, newthread->arch.sp);
+
+#else
     swapcontext(&oldthread->arch.context, &newthread->arch.context);
+#endif
 }
 
 void arch_idle(void) {
